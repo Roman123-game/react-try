e3f07d98a8cbac437898238e7394e52f3d1e0289
RB <82284311+Roman123-game@users.noreply.github.com>
2024-01-23 19:50:45 +0200

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import './App.css';
-import { Obj }  from "./NewObject/Obj"
+import { Obj } from "./NewObject/Obj"
 
 function App(props) {
 
-const groupedObj = Map.groupBy(Obj,person=>person.name);
-console.log(groupedObj)
+  const groupedObj = Map.groupBy(Obj, person => person.name);
+  console.log(groupedObj)
 
   return (
     <div className="App">
-    <p>Hello world</p>
-{Obj.map((ob) => {
-  return (
-  <div  className="container" key={ob.name}>
-<div className="name">{ob.name}</div>
-<div className="age">{ob.age}</div>
-<div className="adress">{ob.adrees}</div>
-<div  className="hobby">{ob.hobby}</div>
-</div>
-  )
-})}
+      <p>Hello world</p>
+      {Obj.map((ob) => {
+        return (
+          <div className="container" key={ob.name}>
+            <div className="name">{ob.name}</div>
+            <div className="age">{ob.age}</div>
+            <div className="adress">{ob.adrees}</div>
+            <div className="hobby">{ob.hobby}</div>
+          </div>
+        )
+      })}
     </div>
   );
 }
