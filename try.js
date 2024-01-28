94951879e03f51df1f4114b9c9da7172d9453106
RB <82284311+Roman123-game@users.noreply.github.com>
2024-01-28 21:26:05 +0200

    update try.js

--- try.js
+++ try.js
@@ -3,7 +3,35 @@ const object1 = {
     b: 42,
     c: false,
   };
+
+
+
+
+  let arr = [1, 2, 3, 4, 5, 6]
+ 
+
+  function fun(){
+    function getSum(total, num) {
+      return total + Math.round(num);
+    }
   
-  console.log(Object.keys(object1));
-  console.log(Object.values(object1));
-  console.log(Object.entries(object1));
+  let first = arr[0]
+  console.log(first,'first')
+  let last = arr[arr.length-1]
+  console.log(last,'last')
+  let removeFirst = arr.filter(item => item !== first)
+  let removeLast = arr.filter(item => item !== last)
+  
+  let sumwitoutLast = removeLast.reduce(getSum, 0)
+  let sumwitoutFirst = removeFirst.reduce(getSum, 0)
+
+  console.log(removeFirst)
+  console.log(removeLast)
+
+ 
+  console.log(sumwitoutLast,sumwitoutFirst)
+  return (sumwitoutLast,sumwitoutFirst)
+  }
+
+
+  fun(arr)
